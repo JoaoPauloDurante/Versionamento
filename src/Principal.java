f25940af2151c319284508a9dbe54b7817bdda97
Maq01 <Maq01@10.66.64.103>
2016-11-28 19:50:48 -0200

    Adicionei o metodo somar

--- src/Principal.java
+++ src/Principal.java
@@ -11,8 +11,15 @@
 public class Principal {
     
     public static void main(String[] args) {
-        
+        somar(5,6);
     }
     
-    
+   static void somar(int n1,int n2){
+   
+   int soma;
+   
+   soma=n1+n2;
+   
+       System.out.println(soma);
+   }  
 }
